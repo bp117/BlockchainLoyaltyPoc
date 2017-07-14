1b9bde6025857fef96f9d6864c31197d690583de
bp117 <bindupriya117@gmail.com>
2017-07-14 09:59:04 +0530

    Update admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,11 +1,11 @@
-var URL = "https://913f1b7b46ae448391fed6eec75b1948-vp0.us.blockchain.ibm.com:5003/chaincode"
+var URL = "https://f29f39c507cd43639862ec95802fbb0e-vp0.us.blockchain.ibm.com:5004/chaincode"
 var myKeyVals =       {
         "jsonrpc": "2.0",
         "method": "query",
         "params": {
             "type": 1,
             "chaincodeID": {
-            "name": "0e11ff13ea3f6eb74252b43b8d3dec09f632f8144d40ce564eb243b59e427def745aca7db9d92be6b5bbd7fad2d6ebde566a82f1a27d6f7900082d1fca2657f4"
+            "name": "030e77eef0cc69ff8a573a102a6f53c208161d4aacd67270dd29c12c3687c71cdc65602b3da2f5d3989238519686689cf311405b61e2caaca6dc1b648ae26458"
             },
             "ctorMsg": {
             "function": "getPoints",
@@ -13,7 +13,7 @@ var myKeyVals =       {
                 "test"
             ]
             },
-            "secureContext": "user_type1_0"
+            "secureContext": "admin"
         },
         "id": 2
     }
